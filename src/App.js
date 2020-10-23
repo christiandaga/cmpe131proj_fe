c3df0f8fc4bab72d6199f8cdf7c4ff71d23ae019
qudu3242 <70551185+qudu3242@users.noreply.github.com>
2020-10-22 20:06:16 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+const express = require('express');
+const morgan = require('morgan')
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const expressHandlebars = require('express-handlebars');
+const flash = require('connect-flash');
+const session = require('express-session');
+const mongoose = require('mongoose')
+const passport = require('passport')
+require('./config/passport')
+mongoose.Promise = global.Promise
+// mongoose.connect('mongodb://localhost:3000') // save it here for database later.
 
 function App() {
   return (
